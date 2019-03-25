345db4c8271c0905f5ec0fa51e3509bbce688efe
Curtis Colwell <ccolwell@uoregon.edu>
2019-03-25 12:41:34 -0700

    script version of proton_opt notebook

---
+++ proton_opt.py
@@ -0,0 +1,14 @@
+import os
+from scripts import *
+
+for file in os.listdir('geometry'):
+    if file.endswith(".xyz"):
+        geometry_filename = 'geometry/' + file
+		
+dummies = []
+for file in os.listdir('dummies'):
+    if file.endswith(".xyz"):
+        dummies.append('dummies/' + file)
+		
+for file in dummies:
+    create_protonopts(geometry_filename, file)
