054d8b66ff3797cd20b81f3bb9a8037e0494f102
CurtisColwell <ccolwell@talapas-ln1.cm.cluster>
2019-11-07 16:27:10 -0800

    Split theory in functionals and basis

--- slurm.bash
+++ slurm.bash
@@ -5,8 +5,9 @@ set -e
 inputname=$(basename -- "$1")
 inputname="${inputname%.*}"
 
-theory="${2:-"M062X/6-31G(d)"}"
-partition="${3:-"short"}"
-processors="${4:-"28"}"
+functional="${2:-"M062X"}"
+basis="${3:-"6-31G**"}"
+partition="${4:-"short"}"
+processors="${5:-"28"}"
 
-sbatch --job-name=$inputname --ntasks=$processors --partition=$partition --output="output/"$inputname".txt" --account=$GROUP_NAME --export=theory=$theory "slurm_submit.srun"
+sbatch --job-name=$inputname --ntasks=$processors --partition=$partition --output="output/"$inputname".txt" --account=$GROUP_NAME --export=functional=$functional,basis=$basis "slurm_submit.srun"
